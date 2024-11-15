1d4d33439efb23d7dfc6e99b003e76389872a48b
Ivan Kirspu <luneyune@github.com>
2024-11-15 21:34:41 +0500

    Сделал базовую аутентификацию пользователя

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import telebot
 from telebot.types import KeyboardButton, ReplyKeyboardMarkup
 
+from intercomAPI import *
+
 import config
 
 # Инициализация бота
 bot = telebot.TeleBot(config.BOT_TOKEN)
+
+interAPI = IntercomAPI(config.DOM_API_TOKEN)
 
 # Создание кнопки с запросом номера телефона
 contact_button = KeyboardButton(text="Отправить номер", request_contact=True)
@@ -26,8 +30,14 @@ def get_text_messages(message):
 @bot.message_handler(content_types=['contact'])
 def handle_contact(message):
     # Получаем номер телефона из сообщения
-    user_phone_number = message.contact.phone_number
-    
+    user_phone_number = message.contact.phone_number[1:]
+
+    try:
+        tenant_id = interAPI.check_tenant(user_phone_number)
+        bot.send_message(message.chat.id, f"Ваш tenant_id = {tenant_id}")
+    except TenantDoesntExist:
+        bot.send_message(message.chat.id, "Вы не зарегистрированы в нашей системе!")
+
     # Отправляем номер обратно пользователю
     bot.send_message(message.chat.id, f"Спасибо! Ваш номер телефона: {user_phone_number}")
 
