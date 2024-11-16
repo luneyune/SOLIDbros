78d62d4c5b635bb1a44d2b31c54d8e46873b1f61
Ivan Kirspu <luneyune@github.com>
2024-11-16 11:01:37 +0500

    Добавил получение списков домофонов

--- main.py
+++ main.py
@@ -30,12 +30,34 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['locations'])
 def get_user_locations(message):
-    tenant_id = dbManager.read_tenant(message.from_user.id)[0]
-    print(tenant_id)
+    tenant = dbManager.read_tenant(message.from_user.id)
+    if not tenant:
+        bot.send_message(message.from_user.id, f"У вас нет локаций...")
+        return
+    tenant_id = tenant[0]
     try:
-        locations = interAPI.domo_apartment(tenant_id)
+        locations = interAPI.tenant_apartments(tenant_id)
         bot.send_message(message.from_user.id, f"ID Ваших локаций: {locations}")
-    except TenantDoesntExist:
+    except ValidationError:
+        bot.send_message(message.from_user.id, f"У вас нет локаций...")
+        return
+    
+@bot.message_handler(commands=['intercoms'])
+def get_user_locations(message):
+    tenant = dbManager.read_tenant(message.from_user.id)
+    if not tenant:
+        bot.send_message(message.from_user.id, f"У вас нет домофонов...")
+        return
+    tenant_id = tenant[0]
+    try:
+        locations = interAPI.tenant_apartments(tenant_id)
+        intercoms = []
+        for loc_id in locations:
+            intercoms_for_loc = interAPI.apartment_intercoms(loc_id, tenant_id)
+            intercoms += intercoms_for_loc
+
+        bot.send_message(message.from_user.id, f"Ваши домофоны: {intercoms}")
+    except ValidationError:
         bot.send_message(message.from_user.id, f"У вас нет локаций...")
         return
 
@@ -52,8 +74,9 @@ def handle_contact(message):
     try:
         tenant_id = interAPI.check_tenant(user_phone_number)
         bot.send_message(message.chat.id, f"Ваш tenant_id = {tenant_id}")
-    except TenantDoesntExist:
+    except ValidationError:
         bot.send_message(message.chat.id, "Вы не зарегистрированы в нашей системе!")
+        return
 
     dbManager.write_tenant(message.from_user.id, tenant_id, user_phone_number)
     # Отправляем номер обратно пользователю
